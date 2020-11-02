2bc27ec98752327325119a15d91e8b53714bc104
Tony Hirst <tony.hirst@gmail.com>
2020-11-02 19:01:56 +0000

    Pypi package test

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 from setuptools import setup
 
+from os import path
+
+def get_long_description():
+    with open(
+        path.join(path.dirname(path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
 
 setup(
     name="nb-tfjs_mnist",
     packages= ['tfjs_mnist'],
-    version='0.0.1',
     include_package_data=True,
+    package_data={
+        "tfjs_mnist": ["static/index.html",
+         "static/mnist_images.png",
+         "static/mnist_labels_uint8",
+         "static/mnist.js",
+         "static/tufte.css"
+         ],
+    },
     install_requires=[
         'jupyter-server-proxy',
         'notebook'
     ],
-    url="https://github.com/ouseful-PR/Hand-Written-Digit-Recognition",
-    author="",
-    description="tony.hirst@gmail.com",
+    url="https://github.com/innovationOUtside/nb_tensorflow_playground_serverproxy",
+    author='Tony Hirst',
+    author_email='tony.hirst@open.ac.uk',
+    description='Jupyter notebook server proxy extension for tensorflow playground.js (MNIST)',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     entry_points={
         'jupyter_serverproxy_servers': [
             'tfjs_mnist = tfjs_mnist:setup_tfjs_mnist',
